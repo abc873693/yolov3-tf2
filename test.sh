549d3e6fe4efa93ca78e0998dfd4bdcdf1531fee
Rainvisitor <abc873693@gmail.com>
2020-03-27 07:39:52 +0800

    Update test only use one GPU

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 
 #declare -a experitentDir=("20190712" "20190715" "20190716" "20190718" "20190719" "20190720" "20190722-1" "20190722-2" "20190723-1" "20190723-2" "20190723-3")
-declare -a experitentDirs=("20191212" "20200104_1" "20200104_2" "20200104_3" "20200108" "20200109_1" "20200109_2" "20200109_3" "20200109_4")
+declare -a experitentDirs=("20200326_3")
 #declare -a dataset=("brown128" "green859" "thickBrown1227" "thinGreen1068")
-declare -a dataset=("microfield_5_v2_test" "microfield_6_test")
+declare -a dataset=("microfield_monocular_train")
 
-weightName=last
+weightName=best
 
 rm records.txt
 
@@ -15,6 +15,6 @@ do
     for data in "${dataset[@]}"
     do
         echo "--------------$data--------------"
-        python3 detect_dataset.py --weights_postfix $weightName --classes ./data/shrimp.names  --tiny --num_classes 1 --dataset $data --experiment $experitentDir --map
+        CUDA_VISIBLE_DEVICES=0 python3 detect_dataset.py --weights_postfix $weightName --classes ./data/shrimp.names  --tiny --num_classes 1 --dataset $data --experiment $experitentDir --map
     done
 done
